af29a49bdfee82f600491cf006345af69df46b6b
Domenica Vizcarra <domeviz3410@gmail.com>
2023-06-05 15:11:40 -0500

    Deber: Pedidos Online

---
+++ app/src/main/java/com/example/dm_proyecto2_pedidosonline/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.dm_proyecto2_pedidosonline
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
