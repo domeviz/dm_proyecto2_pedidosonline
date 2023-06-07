b6b746a0a576c1cf6d5e19105a03e67e70e2e8c6
LABORATORIO DE COMPUTADORAS FACULTAD <fing.labcom@uce.edu.ec>
2023-06-06 19:38:21 -0500

    Menu de navegacion en la Segunda Activity

--- app/src/main/java/com/example/dm_proyecto2_pedidosonline/ui/activities/SegundaActivity.kt
+++ app/src/main/java/com/example/dm_proyecto2_pedidosonline/ui/activities/SegundaActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.util.Log
 import com.example.dm_proyecto2_pedidosonline.R
 import com.example.dm_proyecto2_pedidosonline.databinding.ActivitySegundaBinding
+import com.google.android.material.snackbar.Snackbar
 
 class SegundaActivity : AppCompatActivity() {
 
@@ -19,15 +20,15 @@ class SegundaActivity : AppCompatActivity() {
 
     override fun onStart() {
         super.onStart()
-        var name:String=" "
-        intent.extras.let{
-            name=it?.getString("var1")!!
-        }
-        Log.d("UCE","Hola ${name}")
-        binding.textView.text="Bienvenida "+name.toString()
-        Log.d("UCE","Entrando a Start")
-        binding.back.setOnClickListener{
-            var intent=Intent(this,MainActivity::class.java)
+        var name: String = " "
+//        intent.extras.let{
+//            name=it?.getString("var1")!!
+//        }
+        Log.d("UCE", "Hola ${name}")
+        binding.textView.text = "Bienvenida " + name.toString()
+        Log.d("UCE", "Entrando a Start")
+        binding.back.setOnClickListener {
+            var intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
         initClass()
@@ -40,6 +41,38 @@ class SegundaActivity : AppCompatActivity() {
                 MainActivity::class.java
             )
             startActivity(intent)
+        }
+        binding.bottomNavigation.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.inicio -> {
+                    // Respond to navigation item 1 click
+                    Snackbar.make(
+                        binding.bottomNavigation, "Holi",
+                        Snackbar.LENGTH_LONG
+                    ).show()
+                    true
+                }
+
+                R.id.fav -> {
+                    // Respond to navigation item 2 click
+                    var suma:Int=0
+                    for(i in listOf(9,2,3)){
+                        suma+=i
+                    }
+                    Snackbar.make(
+                        binding.bottomNavigation, "La suma es ${suma}",
+                        Snackbar.LENGTH_LONG
+                    ).show()
+                    true
+                }
+
+                R.id.api -> {
+                    // Respond to navigation item 3 click
+                    true
+                }
+
+                else -> false
+            }
         }
     }
 }
